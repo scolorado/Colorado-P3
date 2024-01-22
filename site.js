4fc4a429dc24c6274020fa16f06c48902f960478
scolorado <224scolorado@frshd.com>
2024-01-22 10:42:15 -0500

    created the variables properly

--- site.js
+++ site.js
@@ -34,6 +34,9 @@ const vue_app = Vue.createApp({
             // This holds your movies.json data.
             movies: [],
             /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+            var title = ref("IMDB + Sofia's Top 8 Movies");
+            var owner = ref("Sofia");
+            var github = ;
          
       }
     },
