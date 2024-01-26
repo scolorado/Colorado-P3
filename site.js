d3706bad8ab31beaba0f81b4f89cbbbb8d8b3eb0
scolorado <224scolorado@frshd.com>
2024-01-26 10:42:20 -0500

    added getmonth function

--- site.js
+++ site.js
@@ -20,76 +20,35 @@
 // FOR STEP 16, ADD THREE OF YOUR OWN FAVORITE MOVIES WITH METADATA TO THE END OF THE JSON FILE LIST
 */
 
-
 const vue_app = Vue.createApp({
-      // This automatically imports your movies.json file and puts it into
-      //   the variable: movies
-      created () {
-            fetch('movies.json').then(response => response.json()).then(json => {
-                  this.movies = json
-            })
-      },
-      data() {
-        return {
-            // This holds your movies.json data.
-            movies: [],
-            /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
-            Title: "IMDB + Sofia's Top 8 Movies",
-            owner: "Sofia",
-            github: "https://github.com/scolorado/Colorado-P3"
-         
-      }
-    },
-      methods: {
-            /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
-            getMonthText(dateArray) {
-              const monthNumber = dateArray[1];
-              let monthText = "";
-                      switch (new Date().getMonth()) {
-                        case 0:
-                          month = "January";
-                          break;
-                        case 1:
-                          month = "Febuary";
-                          break;
-                        case 2:
-                          month = "March";
-                          break;
-                        case 3:
-                          month = "April";
-                          break;
-                        case 4:
-                          month = "May";
-                          break;
-                        case 5:
-                          month = "June";
-                          break;
-                        case  6:
-                          month = "July";
-                          break;
-                        case  7:
-                          month = "August";
-                          break;
-                        case  8:
-                          month = "September";
-                          break;
-                        case  9:
-                          month = "October";
-                          break;
-                        case  10:
-                          month = "November";
-                          break;
-                        case 11:
-                          month = "December";
-                      }
+  // This automatically imports your movies.json file and puts it into
+  //   the variable: movies
+  created () {
+        fetch('movies.json').then(response => response.json()).then(json => {
+              this.movies = json
+        })
+  },
+  data() {
+    return {
+        // This holds your movies.json data.
+        movies: [],
+        /* ADD ADDITIONAL VARIABLES FOR STEP 3 HERE */
+        Title: "IMDB + Sofia's Top 8 Movies",
+        owner: "Sofia",
+        github: "https://github.com/scolorado/Colorado-P3"
+     
+  }
+},
+  methods: {
+        /* ADD FUNCTIONS/METHODS FOR STEP 7 HERE */
+        getMonthText(dateArray) {
+            var year = dateArray[0];
+            var day = dateArray[2];
+            return month + ' ' + day + ', ' + year;
+        }
 
 
-
-                      return monthText + day + ", " + year;
-            }
-
-
-      }
+  }
 })
 
 vue_app.mount("#vue_app")
